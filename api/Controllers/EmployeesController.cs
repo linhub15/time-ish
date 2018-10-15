a6a8f9009e6d816c689baa6e06148ea6367d3824
Hubert Lin <linhub15@gmail.com>
2018-10-15 12:29:56 -0600

    Employee API - working

--- api/Controllers/EmployeesController.cs
+++ api/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using api.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +20,42 @@ namespace api.Controllers
         public ActionResult<IEnumerable<Employee>> Get()
         {
             return _context.Employees;
+        }
+
+        // GET api/employees/:id
+        [HttpGet("{id}")]
+        public ActionResult<Employee> Get(int id)
+        {
+            return _context.Employees
+                .SingleOrDefault(employee => employee.Id == id);
+        }
+
+        // POST api/employees
+        [HttpPost]
+        public ActionResult<int> Post([FromBody] Employee employee)
+        {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+            return employee.Id;
+        }
+
+        // PUT api/employees/:id
+        [HttpPut("{id}")]
+        public ActionResult<Employee> Put(int id, [FromBody] Employee employee)
+        {
+            employee.Id = id;
+            _context.Employees.Update(employee);
+            _context.SaveChanges();
+            return employee;
+        }
+
+        // DELETE /api/employees/:id
+        [HttpDelete("{id}")]
+        public void Delete(int id) 
+        {
+            var Employee = _context.Employees.SingleOrDefault(e => e.Id == id);
+            _context.Employees.Remove(Employee);
+            _context.SaveChanges();
         }
     }
 }
